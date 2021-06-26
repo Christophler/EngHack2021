5fb5c1aa31685e19e9e57c171423e2ee6f056a11
Kevin Ma <kevinma@Kevins-MacBook-Pro.local>
2021-06-26 15:06:22 -0400

    added api request

--- crud-api/src/index.js
+++ crud-api/src/index.js
@@ -1,8 +1,28 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
+import https from 'https';
 
 const app = express();
+
+const options = {
+    hostname: 'https://api.openweathermap.org',
+    path: '/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}',
+    method: 'GET'
+}
+
+https.get('https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=128eacb0a4bcd1d2a7e3ba93dcbf716f', (res) => {
+    let data = '';
+    res.on('data', (chunk) => {
+        data += chunk;
+    });
+
+    //res.on('end', () => {
+    //    console.log(data);
+    //});
+})
+
+console.log(data);
 
 app.use(cors());
 
