5e7a35c6f5fdf410c5afbe73d4ac2f7871c508f8
Kevin Ma <kevinma@Kevins-MacBook-Pro.local>
2021-06-26 22:55:17 -0400

    added functions for DOM

--- frontend/index.js
+++ frontend/index.js
@@ -1,11 +1,10 @@
-
 var map = L.map('map').setView([49.2827, -123.1207], 10);
-L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=uSb3pbKtGymmXLDpb0XX',{
-tileSize: 512,
-zoomOffset: -1,
-minZoom: 1,
-attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
-crossOrigin: true
+  L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=uSb3pbKtGymmXLDpb0XX',{
+  tileSize: 512,
+  zoomOffset: -1,
+  minZoom: 1,
+  attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
+  crossOrigin: true
 }).addTo(map);
 
 var coordinates = 0;
@@ -19,16 +18,29 @@ function getLocation() {
   }
 }
 
+
+
 function onMapClick(e) {
   coordinates = e.latlng;
   location_package = {
     "lat": String(coordinates.lat),
     "lng": String(coordinates.lng),
   }
+
+  function get_api(callback) {
+    var obj;
+    fetch('http://localhost:3000/coordinates', { method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(location_package) })
+      .then(res => res.json())
+      .then(data => obj = data)
+      .then(() => callback(obj));
+  }
   
-  fetch('http://localhost:3000/coordinates', { method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(location_package) })
-    .then(res => res.text())
-    .then(data => console.log(data));
+  function get_data(json) {
+    console.log(json);
+    //Enter DOM here
+  }
+
+  get_api(get_data);
 
   document.getElementById("Latitude").innerHTML = coordinates.lat;
   document.getElementById("Longitude").innerHTML = coordinates.lng;
